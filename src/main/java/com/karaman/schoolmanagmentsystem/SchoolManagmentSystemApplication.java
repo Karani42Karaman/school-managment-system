3c2b12462f60de6e33901aeffdf8d38cf9f5489c
Karani <karamankarani42@gmail.com>
2021-11-14 01:14:50 +0300

    init

---
+++ src/main/java/com/karaman/schoolmanagmentsystem/SchoolManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.karaman.schoolmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karaman/schoolmanagmentsystem/SchoolManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karaman.schoolmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
